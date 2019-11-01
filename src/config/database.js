a6b4b4cd45685e4155fa79edf920df2a5eb977a1
= <=>
2019-11-01 13:05:29 -0300

    Alterado banco

--- src/config/database.js
+++ src/config/database.js
@@ -3,7 +3,7 @@ module.exports = {
   host: 'localhost',
   username: 'postgres',
   password: 'proesc',
-  database: 'gobarber',
+  database: 'gympoint',
   define: {
     timestamps: true,
     underscored: true,
